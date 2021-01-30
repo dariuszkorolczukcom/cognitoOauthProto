fdab1e774d0f4b74b1fee188472e8eec93e30e16
dariusz.korolczuk <dariusz.korolczuk@redseven.tech>
2021-01-30 10:51:47 +0000

    prettify

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import React, { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Link,
-  Redirect,
-  useLocation
-} from "react-router-dom";
-const axios = require('axios');
-
-// React Router does not have any opinions about
-// how you should parse URL query strings.
-//
-// If you use simple key=value query strings and
-// you do not need to support IE 11, you can use
-// the browser's built-in URLSearchParams API.
-//
-// If your query strings contain array or object
-// syntax, you'll probably need to bring your own
-// query parsing function.
+import { BrowserRouter as Router, Link, useLocation } from "react-router-dom";
+const axios = require("axios");
 
 export default function QueryParamsExample() {
   return (
@@ -26,8 +10,6 @@ export default function QueryParamsExample() {
   );
 }
 
-// A custom hook that builds on useLocation to parse
-// the query string for you.
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
@@ -39,7 +21,7 @@ function QueryParamsDemo() {
     <div>
       <div>
         <h2>Accounts</h2>
-            <Link to="/login"></Link>
+        <Link to="/login"></Link>
 
         <Child code={query.get("code")} />
       </div>
@@ -49,63 +31,75 @@ function QueryParamsDemo() {
 
 const config = {
   headers: {
-    'Authorization': "Basic " + btoa(process.env.REACT_APP_OAUTH_CLIENT_ID+":"+process.env.REACT_APP_OAUTH_CLIENT_SECRET),
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
-}
+    Authorization:
+      "Basic " +
+      btoa(
+        process.env.REACT_APP_OAUTH_CLIENT_ID +
+          ":" +
+          process.env.REACT_APP_OAUTH_CLIENT_SECRET
+      ),
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+};
 
 function Child(props) {
   const [token, setToken] = useState("");
 
   useEffect(() => {
-    const params = new URLSearchParams()
-    params.append('grant_type', 'authorization_code')
-    params.append('client_id', process.env.REACT_APP_OAUTH_CLIENT_ID)
-    params.append('code', props.code)
-    params.append('redirect_uri', process.env.REACT_APP_LOGIN_URL)
-    const url = process.env.REACT_APP_OAUTH_URL+'/oauth2/token'
+    const params = new URLSearchParams();
+    params.append("grant_type", "authorization_code");
+    params.append("client_id", process.env.REACT_APP_OAUTH_CLIENT_ID);
+    params.append("code", props.code);
+    params.append("redirect_uri", process.env.REACT_APP_LOGIN_URL);
+    const url = process.env.REACT_APP_OAUTH_URL + "/oauth2/token";
 
-    axios.post(url, params, config)
-  .then((result) => {
-    console.log(result.data.id_token)
-    setToken(result.data.id_token)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-
-  },[]);
-
+    axios
+      .post(url, params, config)
+      .then((result) => {
+        console.log(result.data.id_token);
+        setToken(result.data.id_token);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div>
       {props.code ? (
         <>
-        <h3>
-          The <code>code</code> is &quot;{props.code}
-          &quot;
-        </h3>
-        <h3>
-          The <code>token</code> is &quot;{token}
-          &quot;
-        </h3>
-        <a href={process.env.REACT_APP_OAUTH_URL +
-          "/logout?client_id=" +
-          process.env.REACT_APP_OAUTH_CLIENT_ID + 
-          "&logout_uri=" + 
-          process.env.REACT_APP_LOGIN_URL}>
-          logout
-        </a>
+          <h3>
+            The <code>code</code> is &quot;{props.code}
+            &quot;
+          </h3>
+          <h3>
+            The <code>token</code> is &quot;{token}
+            &quot;
+          </h3>
+          <a
+            href={
+              process.env.REACT_APP_OAUTH_URL +
+              "/logout?client_id=" +
+              process.env.REACT_APP_OAUTH_CLIENT_ID +
+              "&logout_uri=" +
+              process.env.REACT_APP_LOGIN_URL
+            }
+          >
+            logout
+          </a>
         </>
       ) : (
         <>
-          
-          <a href={process.env.REACT_APP_OAUTH_URL +
-            "/login?response_type=code&client_id=" +
-            process.env.REACT_APP_OAUTH_CLIENT_ID +
-            "&redirect_uri=" + 
-            process.env.REACT_APP_LOGIN_URL}>
-          you must login
+          <a
+            href={
+              process.env.REACT_APP_OAUTH_URL +
+              "/login?response_type=code&client_id=" +
+              process.env.REACT_APP_OAUTH_CLIENT_ID +
+              "&redirect_uri=" +
+              process.env.REACT_APP_LOGIN_URL
+            }
+          >
+            you must login
           </a>
         </>
       )}
