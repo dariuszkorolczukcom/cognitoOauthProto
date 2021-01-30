d321a7db0c4ceb31961ec44c55315127b855098b
dariusz.korolczuk <dariusz.korolczuk@redseven.tech>
2021-01-30 13:55:32 +0000

    some extra

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,66 +21,75 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: 20,
     textAlign: "center",
+    justifyContent: "center",
     color: theme.palette.text.secondary,
+    overflowWrap: "break-word",
+  },
+  paperLeftAlign: {
+    padding: 50,
+    textAlign: "left",
+    color: theme.palette.text.primary,
     overflowWrap: "break-word",
   },
   table: {
     minWidth: 650,
   },
+  button: {
+    margin: 5,
+  },
 }));
-
-const axios = require("axios");
-
-export default function QueryParamsExample() {
-  return (
-    <Router>
-      <QueryParamsDemo />
-    </Router>
-  );
-}
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
 
-function QueryParamsDemo() {
-  let query = useQuery();
-
+export default function App() {
   return (
-    <div>
-      <div>
-        <h2>Accounts</h2>
-        <Link to="/login"></Link>
-
-        <Child code={query.get("code")} />
-      </div>
-    </div>
+    <Router>
+      <LoginApp />
+    </Router>
   );
 }
 
-const config = {
-  headers: {
-    Authorization:
-      "Basic " +
-      btoa(
-        process.env.REACT_APP_OAUTH_CLIENT_ID +
-          ":" +
-          process.env.REACT_APP_OAUTH_CLIENT_SECRET
-      ),
-    "Content-Type": "application/x-www-form-urlencoded",
-  },
-};
-
-function Child(props) {
+function LoginApp() {
   const classes = useStyles();
   const [token, setToken] = useState("");
+  const [code, setCode] = useState("");
   const [userData, setUserData] = useState("");
+  let query = useQuery();
 
   useEffect(() => {
+    setCode(query.get("code"));
+    var cookieToken = localStorage.getItem("token");
+    if (cookieToken != "") {
+      setToken(cookieToken);
+      setUserData(jwt_decode(cookieToken));
+    } else {
+      getToken();
+    }
+  }, []);
+
+  const clearCookie = () => {
+    localStorage.setItem("token", "");
+  };
+
+  const getToken = () => {
+    const config = {
+      headers: {
+        Authorization:
+          "Basic " +
+          btoa(
+            process.env.REACT_APP_OAUTH_CLIENT_ID +
+              ":" +
+              process.env.REACT_APP_OAUTH_CLIENT_SECRET
+          ),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
     const params = new URLSearchParams();
     params.append("grant_type", "authorization_code");
     params.append("client_id", process.env.REACT_APP_OAUTH_CLIENT_ID);
-    params.append("code", props.code);
+    params.append("code", query.get("code"));
     params.append("redirect_uri", process.env.REACT_APP_LOGIN_URL);
     const url = process.env.REACT_APP_OAUTH_URL + "/oauth2/token";
 
@@ -89,25 +99,74 @@ function Child(props) {
         setToken(result.data.id_token);
         localStorage.setItem("token", result.data.id_token);
         setUserData(jwt_decode(result.data.id_token));
-        console.log(jwt_decode(result.data.id_token));
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
-
-  const clearCookie = () => {
-    localStorage.setItem("token", "");
   };
 
   return (
     <div className={classes.root}>
-      {props.code ? (
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
+      <Grid container spacing={3}>
+        <Grid item xs={3}></Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paperLeftAlign}>
+            <h1>Cognito Oauth Prototype</h1>
+            <hr />
+            <h3>On First Login</h3>
+            <p>
+              Logging in using existing credentials will triger user creation,
+              and copy some essential values to use in the Cognito JWT claims.
+            </p>
+            <h3>Flow</h3>
+            <ol>
+              <li>
+                Login button on react app takes you to cognito login screen
+              </li>
+              <li>
+                After successfull user authentication user is redirected back to
+                the react app with code used to receive the JWT.
+              </li>
+              <li>
+                The code is sent to the Cognito Authorization endpoint and
+                returns the JWT.
+              </li>
+              <li>
+                The JWT is saved to local storage and processed by jwt_decode to
+                view claims
+              </li>
+              <li>
+                After refreshing the screen the token will be loaded from
+                localStorage
+              </li>
+              <li>
+                Logout button clears token from localStorage and takes user to
+                Cognito logout endpoint
+              </li>
+              <li>
+                User is redirected by the Cognito logout endpoint back to react
+                app
+              </li>
+            </ol>
+            <br />
+            <Button
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              href={
+                process.env.REACT_APP_OAUTH_URL +
+                "/login?response_type=code&client_id=" +
+                process.env.REACT_APP_OAUTH_CLIENT_ID +
+                "&redirect_uri=" +
+                process.env.REACT_APP_LOGIN_URL
+              }
+            >
+              Login
+            </Button>
             <Button
               variant="contained"
               color="secondary"
+              className={classes.button}
               onClick={clearCookie}
               href={
                 process.env.REACT_APP_OAUTH_URL +
@@ -119,72 +178,55 @@ function Child(props) {
             >
               Logout
             </Button>
-          </Grid>
-
-          <Grid item xs={3}></Grid>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <code>code</code>
-              <hr />
-              {props.code}
-            </Paper>
-          </Grid>
-          <Grid item xs={3}></Grid>
-          <Grid item xs={3}></Grid>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <code>JWT</code>
-              <hr />
-              {token}
-            </Paper>
-          </Grid>
-          <Grid item xs={3}></Grid>
-          <Grid item xs={3}></Grid>
-          <Grid item xs={6}>
-            <TableContainer component={Paper}>
-              <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="right">Key</TableCell>
-                    <TableCell align="right">Value</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {Object.keys(userData).map((k) => {
-                    return (
-                      <TableRow key={k}>
-                        <TableCell align="right">{k}</TableCell>
-                        <TableCell align="right">
-                          {String(userData[k])}
-                        </TableCell>
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Grid>
-          <Grid item xs={3}></Grid>
+          </Paper>
         </Grid>
-      ) : (
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Button
-              variant="contained"
-              color="primary"
-              href={
-                process.env.REACT_APP_OAUTH_URL +
-                "/login?response_type=code&client_id=" +
-                process.env.REACT_APP_OAUTH_CLIENT_ID +
-                "&redirect_uri=" +
-                process.env.REACT_APP_LOGIN_URL
-              }
-            >
-              Login
-            </Button>
-          </Grid>
-        </Grid>
-      )}
+        <Grid item xs={3}></Grid>
+        {
+          <>
+            <Grid item xs={3}></Grid>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>
+                <code>Authorization Code for AWS Cognito</code>
+                <hr />
+                {code}
+                <br />
+                <br />
+                <code>JWT from AWS Cognito</code>
+                <hr />
+                {token}
+              </Paper>
+            </Grid>
+            <Grid item xs={3}></Grid>
+            <Grid item xs={3}></Grid>
+            <Grid item xs={6}>
+              User Data from JWT claims:
+              <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell align="left">Key</TableCell>
+                      <TableCell align="left">Value</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {Object.keys(userData).map((k) => {
+                      return (
+                        <TableRow key={k}>
+                          <TableCell align="left">{k}</TableCell>
+                          <TableCell align="left">
+                            {String(userData[k])}
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+            <Grid item xs={3}></Grid>
+          </>
+        }
+      </Grid>
     </div>
   );
 }
